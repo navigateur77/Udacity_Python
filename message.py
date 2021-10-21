c143eefe2126b097c36ca33b0a7d053ef1687ffc
navigateur77 <nj.compaore@gmail.com>
2021-10-21 00:22:11 -0400

    improuved draft

--- message.py
+++ message.py
@@ -1,27 +1,19 @@
 # get and process input for a list of names
-names =  input('Enter names separated by commas: ')
-names = list(names.split(','))
-
-print(names)
+names =  input('Enter names separated by commas: ').title().split(',')
 
 # get and process input for a list of the number of assignments
 
-assignments =  input('Enter assignments counts separatedby commas: ')
-assignments = list(assignments.split(','))
-print(assignments)
+assignments =  input('Enter assignments counts separatedby commas: ').split(',')
 
 # get and process input for a list of grades
 
-grades =  input('Enter grades separated by commas: ')
-grades = list(grades.split(','))
-print(grades)
+grades =  input('Enter grades separated by commas: ').split(',')
 
 # message string to be used for each student
 # HINT: use .format() with this string in your for loop
 
+message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
+submit before you can graduate. You're current grade is {} and can increase \
+to {} if you submit all assignments before the due date.\n\n"
 for n, a, g in zip(names, assignments, grades):
-	message = "Hi {},\n\nThis is a reminder that you have {} assignments left to \
-	submit before you can graduate. You're current grade is {} and can increase \
-	to {} if you submit all assignments before the due date.\n\n".format(n, int(a), int(g), (2*int(a) + int(g)))
- 
-	print(message)
+	print(message.format(n, a, int(g), int(a * 2)))
